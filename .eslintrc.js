51ed15d5136631ad863504b760152e07e8285732
David Dalcino <ddalcino@gmail.com>
2021-10-04 20:27:33 -0700

    WIP setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["old/**/*", "temp.js", "**/vendor/*.js"],
   rules: {
     "prettier/prettier": "error",
   },
