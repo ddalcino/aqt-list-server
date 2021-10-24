b7bc322c8eaa0e4eff3f883b8fea69b18b483fd7
David Dalcino <ddalcino@gmail.com>
2021-10-24 11:35:22 -0700

    Fix list-qt implementation

--- src/aqt-list-qt-ts/list-qt.ts
+++ src/aqt-list-qt-ts/list-qt.ts
@@ -13,28 +13,26 @@ import {
   to_versions,
   to_archives,
 } from "./list-qt-impl";
+import Result from "../lib/Result";
 
-class Http404 extends Error {}
+class FetchError extends Error {}
+type FetchResult<T> = Result<T, FetchError>;
 
 const generic_fetch_data = <T, U, V>(
   url_func: (args: U) => string,
   xml_transform: (xml: string, args: V) => T
-): ((url_args: U, xml_args: V) => Promise<T>) => {
+): ((url_args: U, xml_args: V) => Promise<FetchResult<T>>) => {
   return async (url_args: U, xml_args: V) => {
     const url = url_func(url_args);
-    return fetch(url)
-      .then((response: Response) => {
-        if (response.status === 404) throw new Http404("Http 404!");
-        if (response.status !== 200) {
-          throw new Error(`Failed HTTP fetch with status ${response.status}`);
-        }
-        return response.text();
-      })
-      .then((xml: string) => xml_transform(xml, xml_args))
-      .catch((ex) => {
-        console.log(ex);
-        throw new Error(`Failed HTTP fetch: ${ex}`);
-      });
+    const response = await fetch(url, { cache: "force-cache" });
+    if (response.status !== 200) {
+      return Result.Err<T, FetchError>(
+        new FetchError(`Bad fetch status: ${response.status}`)
+      );
+    }
+    return Result.Ok<T, FetchError>(
+      xml_transform(await response.text(), xml_args)
+    );
   };
 };
 
@@ -45,7 +43,7 @@ export const fetch_versions = (
   generic_fetch_data<string[][], [Host, Target], []>(to_url, to_versions)(
     [host, target],
     []
-  );
+  ).then((result) => result.unwrap());
 
 export const fetch_arches = async (
   host: Host,
@@ -53,19 +51,17 @@ export const fetch_arches = async (
   version: SemVer
 ): Promise<string[]> => {
   const [without_wasm, with_wasm] = await Promise.all(
-    [to_qt_nowasm_updates_xml, to_qt_wasm_updates_xml].map((url_generator) => {
-      try {
-        return generic_fetch_data<string[], [Host, Target, SemVer], []>(
-          url_generator,
-          to_arches
-        )([host, target, version], []);
-      } catch (ex) {
-        if (ex instanceof Http404) {
-          return [];
-        }
-        throw ex;
-      }
-    })
+    [to_qt_nowasm_updates_xml, to_qt_wasm_updates_xml].map((url_generator) =>
+      generic_fetch_data<string[], [Host, Target, SemVer], [SemVer]>(
+        url_generator,
+        to_arches
+      )([host, target, version], [version]).then((result) =>
+        result.match(
+          (arches: string[]): string[] => arches,
+          (_err: FetchError): string[] => []
+        )
+      )
+    )
   );
   return without_wasm.concat(with_wasm);
 };
@@ -76,10 +72,10 @@ export const fetch_modules = (
   version: SemVer,
   arch: string
 ): Promise<string[]> =>
-  generic_fetch_data<string[], [Host, Target, SemVer], [string]>(
+  generic_fetch_data<string[], [Host, Target, SemVer], [SemVer, string]>(
     to_qt_updates_xml(arch),
     to_modules
-  )([host, target, version], [arch]);
+  )([host, target, version], [version, arch]).then((result) => result.unwrap());
 
 export const fetch_archives = async (
   host: Host,
@@ -88,16 +84,20 @@ export const fetch_archives = async (
   arch: string,
   modules: string[]
 ): Promise<string[]> =>
-  generic_fetch_data<string[], [Host, Target, SemVer], [string, string[]]>(
-    to_qt_updates_xml(arch),
-    to_archives
-  )([host, target, version], [arch, modules]);
+  generic_fetch_data<
+    string[],
+    [Host, Target, SemVer],
+    [SemVer, string, string[]]
+  >(to_qt_updates_xml(arch), to_archives)(
+    [host, target, version],
+    [version, arch, modules]
+  ).then((result) => result.unwrap());
 
 export const fetch_tools = (host: Host, target: Target): Promise<string[]> =>
   generic_fetch_data<string[], [Host, Target], []>(to_url, to_tools)(
     [host, target],
     []
-  );
+  ).then((result) => result.unwrap());
 
 export const fetch_tool_variants = (
   host: Host,
@@ -107,4 +107,4 @@ export const fetch_tool_variants = (
   generic_fetch_data<ToolVariant[], [Host, Target, string], []>(
     to_tools_updates_xml,
     to_tool_variants
-  )([host, target, tool_name], []);
+  )([host, target, tool_name], []).then((result) => result.unwrap());
